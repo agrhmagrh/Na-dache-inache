35c7c23721a02b7a289c714c11bab4c97767d1f2
agrhmagrh <agrhmagrh@duck.com>
2024-04-26 02:47:36 +0300

    Работа на адаптивным дизайном главной страницы

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -9,8 +9,8 @@ const classes = "card relative" + (columns > 1 ? " col-span-" + columns : "")
 
   return (
     <div className={classes}>
-      <Image width={width} height={0}  src={src} alt={title}></Image>
-      <Link className="absolute top-1/2 text-nowrap m-auto px-10 py-5 min-w-[250px] -translate-x-1/2 -translate-y-1/2 left-1/2 text-center bg-black-70 text-white" href={link}>{title.toUpperCase()}</Link>
+      <Image className={"w-full scale-10"} width={width} height={0}  src={src} alt={title}></Image>
+      <Link className="absolute top-1/2 text-nowrap m-auto px-10 py-5 min-w-[250px] -translate-x-1/2 -translate-y-1/2 left-1/2 text-center bg-black-70 text-white text-sm md:text-base" href={link}>{title.toUpperCase()}</Link>
     </div>
   );
 }
