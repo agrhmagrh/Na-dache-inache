8b46f69ebcafa2b0828e388052c97f98868aaae2
agrhmagrh <agrhmagrh@duck.com>
2024-05-01 01:55:53 +0300

    Мобильное меню

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -8,7 +8,7 @@ const classes = "card relative" + (columns > 1 ? " col-span-" + columns : "")
   return (
     <div className={classes}>
       <Image className={"w-full"} width={400} height={0}  src={src} alt={title}></Image>
-      <Link className="absolute top-1/2 text-nowrap m-auto px-10 py-5 md:min-w-40 min-w-[250px] xl:min-w-[250px] -translate-x-1/2 -translate-y-1/2 left-1/2 text-center bg-black-70 text-white text-sm xl:text-base" href={link}>{title.toUpperCase()}</Link>
+      <Link className="absolute top-1/2 md:text-nowrap m-auto px-10 py-5 md:min-w-40 min-w-[250px] xl:min-w-[250px] -translate-x-1/2 -translate-y-1/2 left-1/2 text-center bg-black-70 text-white text-sm xl:text-base" href={link}>{title.toUpperCase()}</Link>
     </div>
   );
 }
