577ed172d567ae78d8681132bd8e0f21f53d055e
agrhmagrh <agrhmagrh@duck.com>
2024-06-04 18:23:28 +0300

    Добавлена яндекс метрика и сео описание

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
+import Metrika from "./components/Metrika";
 
 const rubik = Rubik({ subsets: ["latin", "cyrillic"] });
 
 export const metadata: Metadata = {
-  title: "Беседка Лофт>",
-  description: "",
-
+  title: "Беседка Лофт",
+  description: "Наш сайт предлагает широкий выбор беседок для вашего сада или участка. Мы производим качественные и прочные беседки из дерева, металла и других материалов. У нас вы можете найти беседки различных размеров и дизайнов, которые подойдут для отдыха, барбекю или просто приятного времяпровождения на свежем воздухе. Покупайте у нас беседки и создайте уютное место для приятного отдыха с семьей и друзьями.",
 };
 
 export default function RootLayout({
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={rubik.className}>{children}</body>
+        <Metrika />
     </html>
   );
 }
