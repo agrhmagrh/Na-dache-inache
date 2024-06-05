e3de04d6fe6face3f9a9fe183eddd5d291c58177
agrhmagrh <agrhmagrh@duck.com>
2024-06-05 10:26:00 +0300

    Добавлены сео информация, привязка к яндекс вебмастеру

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,11 @@ import { Rubik } from "next/font/google";
 import "./globals.css";
 import Metrika from "./components/Metrika";
 import { Suspense } from "react";
+import { SEO } from "@/seo";
 
 const rubik = Rubik({ subsets: ["latin", "cyrillic"] });
 
-export const metadata: Metadata = {
-  title: "Беседка Лофт",
-  description: "Наш сайт предлагает широкий выбор беседок для вашего сада или участка. Мы производим качественные и прочные беседки из дерева, металла и других материалов. У нас вы можете найти беседки различных размеров и дизайнов, которые подойдут для отдыха, барбекю или просто приятного времяпровождения на свежем воздухе. Покупайте у нас беседки и создайте уютное место для приятного отдыха с семьей и друзьями.",
-};
+export const metadata: Metadata = SEO
 
 export default function RootLayout({
   children,
