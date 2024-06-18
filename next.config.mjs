f7d7707b086440fec079e7f50829015136a8003f
Roman Vilkov <63471256+Roman-Kiriko@users.noreply.github.com>
2024-06-19 02:29:06 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: { unoptimized: true },
-    distDir: 'build',
+    output: "export",
+    images: { unoptimized: true }
 };
 
 export default nextConfig;
