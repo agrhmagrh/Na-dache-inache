6ad4411cb2d3d72da749c6a58c453a46bc797d2d
agrhmagrh <agrhmagrh@duck.com>
2025-02-04 00:28:57 +0300

    Изменение кода в хедере

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,37 +1,52 @@
-"use client"
+"use client";
 import Logo from "./header/Logo";
 import Menu from "./header/Menu";
-import Contact from "./header/Contact"
-import { useEffect, useState } from "react";
+import Contact from "./header/Contact";
+import { useEffect, useState, useCallback, useMemo } from "react";
 import { TypeDesktopProps, TypeMobileProps } from "@/types";
 
-export default function Header({isMain}: TypeDesktopProps | TypeMobileProps) {
-  const [isShowMobileMenu, setShowMobileMenu] = useState(true)
-  useEffect(()=> {
-    const {offsetWidth} = document.body
-    if(offsetWidth > 1200) {
-      setShowMobileMenu(false)
+export default function Header({ isMain }: TypeDesktopProps | TypeMobileProps) {
+  const [isShowMobileMenu, setShowMobileMenu] = useState(true);
 
-    } else {
-      setShowMobileMenu(true)
-    }
-  }, [])
+  const handleResize = useCallback(() => {
+    const { offsetWidth } = document.body;
+    setShowMobileMenu(offsetWidth <= 1200);
+  }, []);
 
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handleResize]);
 
-  return isShowMobileMenu ? <MobileHeader type={"mobile"}/> : <DesktopHeader isMain={isMain} type={"desktop"}/>
+  const HeaderComponent = useMemo(
+    () =>
+      isShowMobileMenu ? (
+        <MobileHeader type="mobile" />
+      ) : (
+        <DesktopHeader isMain={isMain} type="desktop" />
+      ),
+    [isShowMobileMenu, isMain]
+  );
+
+  return HeaderComponent;
 }
 
-function DesktopHeader({type, isMain}: TypeDesktopProps) {
-  return <header className="header bg-white grid grid-cols-12 items-center max-w-screen-2xl m-auto top-0 z-20" >
-  <Logo ></Logo>
-  <Menu type={type}></Menu>
-  <Contact type={type} isMain={isMain}></Contact>
-</header>
+function DesktopHeader({ type, isMain }: TypeDesktopProps) {
+  return (
+    <header className="header bg-white grid grid-cols-12 items-center max-w-screen-2xl m-auto top-0 z-20">
+      <Logo />
+      <Menu type={type}></Menu>
+      <Contact type={type} isMain={isMain}></Contact>
+    </header>
+  );
 }
-function MobileHeader({type}: TypeMobileProps) {
-  return  <header className="header bg-white grid grid-cols-8 items-center w-full m-auto sticky top-0 z-20">
-  <Menu type={type}></Menu>
-  <Logo ></Logo>
-  <Contact type={type}></Contact>
-</header>
+function MobileHeader({ type }: TypeMobileProps) {
+  return (
+    <header className="header bg-white grid grid-cols-8 items-center w-full m-auto sticky top-0 z-20">
+      <Menu type={type}></Menu>
+      <Logo></Logo>
+      <Contact type={type}></Contact>
+    </header>
+  );
 }
