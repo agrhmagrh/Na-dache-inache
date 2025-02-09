ac2d7f6eed2bfd175e706bafe7188a6a1c78e069
agrhmagrh <aurimcreo@gmail.com>
2025-02-10 01:26:08 +0300

    Вынес текст в константы

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,19 @@
 import OffersBlock from "./HomeSections/OffersBlock";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { text } from "./contstants/const";
 
 export default function NotFound() {
-    return (
-        <>
-          <Header />
-          <main className="main bg-gray-light">
-          <div className="apologies p-10 max-w-screen-xl m-auto font-medium text-4xl">
-                Страница отсутствует, но мы уже можем предложить вам другие наши уникальные решения
-            </div>
-            <OffersBlock></OffersBlock>
-          </main>
-          <Footer />
-        </>
-      );
+  return (
+    <>
+      <Header />
+      <main className="main bg-gray-light">
+        <div className="apologies p-10 max-w-screen-xl m-auto font-medium text-4xl">
+          {text.NotFound}
+        </div>
+        <OffersBlock></OffersBlock>
+      </main>
+      <Footer />
+    </>
+  );
 }
